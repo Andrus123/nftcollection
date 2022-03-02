d7e64008f5dba48ff8bf816dfa320746d714f069
Andrés K. Aquin <andruuus123@gmail.com>
2022-03-02 01:01:13 -0400

    first commit

---
+++ hardhat-contracts/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xE92079e234435cA821E302d6abDDE8c317B0218F";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
